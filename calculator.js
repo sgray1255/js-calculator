9fbbd1df0ce31edfda1000e88efb5f20aaa6f718
Stephen Gray <stephen_gray1255@yahoo.com>
2021-10-04 10:13:34 +1100

    I wrote the basic javascript functions for the calculator.

--- calculator.js
+++ calculator.js
@@ -1,61 +1,77 @@
+const displayOne = document.querySelector("#first-operand");
+const displayTwo = document.querySelector("#second-operand");
+const numberButtons = document.querySelectorAll(".button-number");
+const operationButtons = document.querySelectorAll(".button-operator");
+const equal = document.querySelector("#equal");
+const clear = document.querySelector(".clear");
+const allClear = document.querySelector(".all-clear");
 
-const numberButtons = document.getElementsByClassName("button-number");
-const operatorButtons = document.getElementsByClassName("button-operator");
-const equalsButton = document.getElementById("equal");
-const deleteButton = document.getElementById("delete");
 
-const calculator = {
-  displayValue: '',
-  firstOperand: null,
-  currentOperand: false,
-  operator: null,
+let displayOneNumber = '';
+let displayTwoNumber = '';
+let result = parseFloat(null);
+let operation = '';
+let decimal = false;
+
+
+for (let i=0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click", (event) => { 
+    if (event.target.value === '.' && !decimal) {
+      decimal = true;
+    } else if(event.target.value === '.' && decimal) {
+      return;
+    }
+    displayTwoNumber += event.target.value;
+    displayTwo.value = displayTwoNumber;
+    return displayTwoNumber;
+  }
+ )};
+
+for (let i=0; i < operationButtons.length; i++) {
+  operationButtons[i].addEventListener("click", (event) => {
+    const operationName = operationButtons[i].value;
+    if (!displayTwoNumber) {
+      return;
+    }
+    if (displayOneNumber && displayTwoNumber && operation) {
+      calculate();
+    } else {
+      result = displayTwoNumber;
+    }
+    clearDisplay(operationName);
+    operation = operationName;
+    console.log(result);
+});
+
+const clearDisplay = (operationName = ' ') => {
+  displayOneNumber += displayTwoNumber + ' ' + operationName + ' ';
+  displayOne.value = displayOneNumber;
+  displayTwo.value = ' ';
+  displayTwoNumber = ' ';
+}};
+
+const calculate = () => {
+  if (operation === 'x') {
+    result = parseFloat(result) * parseFloat(displayTwoNumber);
+  } else if(operation === '/') {
+    result = parseFloat(result) / parseFloat(displayTwoNumber);
+  } else if(operation === '+') {
+    result = parseFloat(result) + parseFloat(displayTwoNumber);
+  } else if(operation === '-'){
+    result = parseFloat(result) - parseFloat(displayTwoNumber);
+  }
 };
 
-const updateDisplay = () => {
-  const window = document.getElementById("window");
-  window.value = calculator.displayValue;
-}
-
-updateDisplay();
-
-
-for (let i=0; i < numberButtons.length; i++) { numberButtons[i].addEventListener("click", (event) => {
-      const window = document.getElementById("window");
-      window.value += event.target.value;
-      const { displayValue } = calculator;
-      calculator.displayValue = displayValue === '' ? window.value: displayValue + window.value;
-    })
-  };
-
-console.log(calculator.displayValue)
-
-for (let i=0; i < operatorButtons.length; i++) {
-  operatorButtons[i].addEventListener("click", (event) => {
-    const window = document.getElementById("window");
-    
-    if (event.target.value === 'x'){
-      window.value += event.target.value;
-    }
-    if (event.target.value === '/'){
-      window.value += event.target.value;
-    }
-    if (event.target.value === '+'){
-      window.value += event.target.value;
-    }
-    if (event.target.value === '-'){
-      window.value += event.target.value;
-    }
-    if (event.target.value === 'A/C'){
-      updateDisplay();
-    }
-    
-
+equal.addEventListener("click", (event)=>{
+  calculate();
+  displayOne.value = '';
+  displayTwo.value = parseFloat(result);
   });
-}
 
-
-
-// loop through numberButtons and add event listener
-
-
-
+allClear.addEventListener("click", (event)=> {
+  displayOne.value = '';
+  displayTwo.value = '';
+  displayOneNumber = '';
+  displayTwoNumber = '';
+  result = '';
+})
